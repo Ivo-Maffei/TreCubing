608e5c936f2ee05921ef298a320febaf9a1e6ec1
Ivo Maffei <ivomaffei@gmail.com>
2023-11-26 10:39:43 +0000

    removing files that should not be synced

--- old/lowLevelSquaring.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOW_LEVEL_SQUARING_H
-#define LOW_LEVEL_SQUARING_H
-
-#include <gmp.h>
-
-void lowLevelSquaring(mpz_t m, mpz_t p, const unsigned long b, const unsigned long Rlimbs);
-
-#endif

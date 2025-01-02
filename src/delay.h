4850c14e644007c1d72e77f5385a40da4e7a112b
Ivo Maffei <ivomaffei@gmail.com>
2025-01-02 15:54:47 +0100

    Fixed bugs related to using keys in thorp.

--- src/delay.h
+++ src/delay.h
@@ -6,9 +6,9 @@
 #include <stdint.h>
 
 // delay message m using cubing modulo p, chained C times with Thorp shuffle using R rounds of shuffling
-void delay(mpz_t r, const mpz_t m, const mpz_t p, const unsigned long C,  const unsigned long R, const uint64_t randSeed, const uint8_t *key);
+void delay(mpz_t r, const mpz_t m, const mpz_t p, const unsigned long C,  const unsigned long R, const uint8_t *key);
 
 // opens the puzzle c and place the result in m
-void open(mpz_t m, const mpz_t c, const mpz_t p, const mpz_t b, const unsigned long C, const unsigned long R, const uint64_t randSeed, const uint8_t *key);
+void open(mpz_t m, const mpz_t c, const mpz_t p, const mpz_t b, const unsigned long C, const unsigned long R, const uint8_t *key);
 
 #endif

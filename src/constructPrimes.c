4132a0c2f56d07999d57fe86cc1937ab00d3653c
Ivo Maffei <ivomaffei@gmail.com>
2025-01-07 17:25:05 +0100

    findpsuedosafeprimes using Pocklington and up to 5k primes

--- src/constructPrimes.c
+++ src/constructPrimes.c
@@ -7,23 +7,23 @@
 
 const unsigned long availablePrimeSizes[] = {
     512,
-    1000,
     1024,
-    2000,
+    2048,
+    3072,
     5000,
     10000,
-    20000,
+    //    20000,
     30000,
-    40000,
+    // 40000,
     50000,
-    60000,
+    // 60000,
     70000,
-    80000,
+    // 80000,
     90000,
     100000
 };
 
-const unsigned long numAvailablePrimes = 15;
+const unsigned long numAvailablePrimes = 11;
 
 
 
@@ -130,58 +130,45 @@ void constructPrime100k(mpz_t p) {
 // we assume such safe prime is larger than the 10 000th prime
 void findPseudoSafePrime(mpz_t p, const unsigned long Nbits) {
 
-    mpz_t q; // candidate prime
-    bool isPrime = false;
-    int primeCheck = -1;
-    const unsigned long precision = 100l;
+    mpz_t q, t;
+    bool isPrime;
+    unsigned long count = 0;
 
-    mpz_inits(q, NULL);
+    mpz_inits(q, t , NULL);
 
-
-    mpz_set_ui(p, 1l);
-    mpz_mul_2exp(p, p, Nbits-1); // p is 2^{Nbits-1}
+    mpz_set_ui(q, 1l);
+    mpz_mul_2exp(q, q, Nbits-1); // q is 2^{Nbits-1}
 
     isPrime = false;
     while (!isPrime){
+	++count;
+	mpz_nextprime(q, q); // finds the next prime
 
-	mpz_nextprime(p, p); // supposedly finds the next prime
-        mpz_mod_ui(q, p, 3l); // compute q = p % 3
-	// both p and q must be 2 modulo 3
-	if( mpz_cmp_ui(q, 2l) != 0) continue; // not safe prime
+	// we will check if p=2q+1 is prime with the pocklington test
+	// i.e. if there is an integer a such that a^{p-1} = 1 mod p and gcd(a^2-1, p) =1, then p is prime
+	// we do the test for a=2 (we could pick a=3, so that a^2-1 is 8, hence we only need p to be odd, which it is!)
+	// note that if we fail, then q is not prime (either 2^{p-1} != 1 or 3 divides p)
 
-	// check if p is a prime with high prob
-	primeCheck = mpz_probab_prime_p(p, precision);
+	// we check gcd(3, p)=1 by checking q%3 == 2
+	mpz_mod_ui(t, q, 3l); // compute t = q % 3
+	if( mpz_cmp_ui(t, 2l) != 0) continue; // not safe prime
 
-	// assume p = 2q+1 and compute q
-	mpz_sub_ui(q, p, 1l);
-	mpz_divexact_ui(q, q, 2l);
+	// now check 2^{p-1} = 1 mod p
+	// note that t is already 2
+	mpz_mul_2exp(p, q, 1l);
+	mpz_add_ui(p, p, 1l);
+	// p <- 2q+1
 
-	// check if q is prime
-	primeCheck = mpz_probab_prime_p(q, precision);
-	if (primeCheck  != 0) {
+	// we compute 2^p mod p which must be 2
+	mpz_powm(t, t, p, p);
+	if (mpz_cmp_ui(t, 2l) == 0l) { // found a safe prime
 	    isPrime = true;
-	    //printf("Found p prime %i\n", primeCheck);
-	}
-	else {
-	    // perhaps 2p+1 is prime
-	    // set q  to 2p+1
-	    mpz_mul_2exp(q, p, 1l);
-	    mpz_add_ui(q, q, 1l);
-
-	    // check if q is prime
-	    primeCheck = mpz_probab_prime_p(q, precision);
-	    if(primeCheck != 0) {
-		isPrime = true;
-		mpz_set(p, q);
-		//printf("Found p prime %i\n", primeCheck);
-	    }
 	}
 
+	if ( (count << 54) == 0) fprintf(stderr, ".");
     }
-
-    //printf("Size of prime found %lu\n", mpz_sizeinbase(p, 2));
-
-    mpz_clears(q, NULL);
+    fprintf(stderr, "tested %lu primes\n", count);
+    mpz_clears(q, t, NULL);
 }
 
 
@@ -189,12 +176,6 @@ void findPseudoSafePrime(mpz_t p, const unsigned long Nbits) {
 // p should already be initialised with the correct size
 void constructPrime(mpz_t p, const unsigned long N) {
     switch (N) {
-    case 512:
-    case 1024:
-    case 1000l: // fall trough to 2000l
-    case 2000l:
-	findPseudoSafePrime(p, N);
-	break;
     case 5000l:
 	constructPrime5k(p);
 	break;
@@ -229,8 +210,12 @@ void constructPrime(mpz_t p, const unsigned long N) {
 	constructPrime100k(p);
 	break;
     default:
-	fprintf(stderr, "constructing primes of size %lu is not supported\n", N);
-	assert(0);
+	if (N < 5000l)
+	    findPseudoSafePrime(p, N);
+	else {
+	    fprintf(stderr, "constructing primes of size %lu is not supported\n", N);
+	    assert(0);
+	}
     }
 }
 

901f3d5aed0cb217c5b35e4cbb5c109297e51acc
Ivo Maffei <ivo.maffei@uni.lu>
2025-01-30 08:22:00 +0100

    AES-OFB

--- src/enc.h
+++ src/enc.h
@@ -13,6 +13,8 @@ void cycleEnc(mpz_t c, const mpz_t m, const mpz_t p, const uint8_t *key);
 
 void cycleDec(mpz_t m, const mpz_t c, const mpz_t p, const uint8_t *key);
 
+int streamCipher(mpz_t res, const mpz_t m, const uint64_t N, const uint8_t *key);
+
 void cleanOpenSSL();
 
 int initialiseOpenSSL();
